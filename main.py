221cfab25a762ee2f64af86a91bfcb0f36340c7b
vitorzmaluf <vitorzamignanim@gmail.com>
2021-06-01 09:40:01 -0300

    codigo finalizado

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from threading import Thread, Lock
 import random
 from time import sleep
+import math
 
 
 tanqueDeOleo = Lock()
@@ -16,16 +17,33 @@ class Simulador:
     def __init__(self):
         self.quantDentroReator = 0.0
         self.quantDentroDecantador = 0.0
-        self.decantadorDisponivel = True
         self.quantDentroReator = 0.0
         self.quantDentroDecantador = 0.0
         self.quantOleo = 0.0
         self.quantNaOh = 0.0
         self.quantEtOh = 0.0
+        self.solLavagem = 0.0
+        self.quantSecador = 0.0
+        self.biodieselGerado = 0.0
+        self.ciclosReator = 0
+        self.quantGlicerina = 0.0
+
+        self.rodando = True
+        self.decantadorDisponivel = True
+        self.lavadoresDisponiveis = True
+        self.secadorDisponivel = True
+
+        self.t1 = Thread(target = self.tanqOlRec)
+        self.t2 = Thread(target= self.recebeNaOheEtOH)
+        self.t3 = Thread(target=self.ativaReator)
+        self.t4 = Thread(target=self.ativaDecantador)
+        self.t5 = Thread(target=self.lavagem)
+        self.t6 = Thread(target=self.secagem)
+
     # Tanque de oleo recebe oleo
     def tanqOlRec(self):
         self.quantOleo = 0.0
-        while True:
+        while self.rodando:
         #     tanqueDeOleo.acquire()
         #     try:
             tempoReceb = random.randrange(1, 11)
@@ -39,7 +57,7 @@ class Simulador:
     def recebeNaOheEtOH(self):
         self.quantNaOh = 0.0
         self.quantEtOh = 0.0
-        while True:
+        while self.rodando:
             self.quantNaOh += 0.25
             self.quantEtOh += 0.125
             sleep(1)
@@ -50,34 +68,27 @@ class Simulador:
         self.quantDentroReator = 0.0
         self.quantDentroDecantador = 0.0
         self.decantadorDisponivel = True
-        while True:
+        while self.rodando:
             total = self.quantNaOh+self.quantEtOh+self.quantOleo
-            print(total)
             if self.quantEtOh >= 5/2 and self.quantNaOh >= 5/4 and self.quantOleo >= 5/4 and self.decantadorDisponivel:
-                if(total < 5):
-                    if(self.quantDentroDecantador+total<=10):
-                        self.quantDentroReator -= total
-                        self.quantDentroDecantador += total
-                        print("Decantador abastecido")
-                        if(self.quantDentroDecantador==10):
-                            self.decantadorDisponivel = False
-                    else:
-                        print("Decantador não consegue receber", total, "litros")
-                if(total >= 5):
-                    if(self.quantDentroDecantador+5<=10):
-                        self.quantDentroReator -= 5
-                        self.quantDentroDecantador += 5
-                        print("Decantador abastecido")
-                        if(self.quantDentroDecantador==10):
-                            self.decantadorDisponivel = False
-                    else:
-                        print("Decantador não consegue receber 5 litros")
+                if(self.quantDentroDecantador+5<=10):
+                    self.quantDentroReator -= 5
+                    self.quantEtOh -= 5/2
+                    self.quantNaOh -= 5/4
+                    self.quantOleo -= 5/4
+                    self.quantDentroDecantador += 5
+                    self.ciclosReator += 1
+                    print("Decantador abastecido")
+                    if(self.quantDentroDecantador==10):
+                        self.decantadorDisponivel = False
+                else:
+                    print("Decantador não consegue receber 5 litros")
             else:
                 ("Reator - Proporcao incorreta")
             sleep(1)
 
     def ativaDecantador(self):
-        while True:
+        while self.rodando:
             if(self.quantDentroDecantador<10):
                 self.decantadorDisponivel = True
                 #Aguarda 2 segundos a saida do reator
@@ -86,20 +97,58 @@ class Simulador:
                 self.decantadorDisponivel = False
 
                 print("Decantador trabalhando")
-                self.quantDentroDecantador = self.quantDentroDecantador-3
+                self.quantGlicerina = self.quantGlicerina + (3*0.02)
+                self.quantEtOh = self.quantEtOh + (3*0.09)
+                if(self.lavadoresDisponiveis):
+                    self.quantDentroDecantador = self.quantDentroDecantador-3
+                    self.solLavagem += 3*0.89
                 sleep(5)
+    
+    def lavagem(self):
+        while self.rodando:
+            if(self.solLavagem > 0 and self.lavadoresDisponiveis):
+                self.lavadoresDisponiveis = False
+                self.solLavagem = self.solLavagem*0.925
+                self.solLavagem = self.solLavagem*0.925
+                self.solLavagem = self.solLavagem*0.925
+                self.quantSecador = self.solLavagem
+                print("Foram lavados", self.solLavagem, "litros de solução")
+                self.solLavagem = 0
+                self.lavadoresDisponiveis = True
+    
+    def secagem(self):
+        while self.rodando:
+            if(self.quantSecador>0 and self.secadorDisponivel):
+                self.quantSecador = self.quantSecador*0.97
+                for i in range(0, int(math.ceil(self.quantSecador))):
+                    sleep(5)
+                    print("1 litro de solução foi secada")
+                    self.quantSecador -= 1
+                    self.biodieselGerado += 1
+                    print("Total de Biodiesel gerado:", self.biodieselGerado)
+                    
+    def iniciar(self):
+        self.t1.start()
+        self.t2.start()
+        self.t3.start()
+        self.t4.start()
+        self.t5.start()
+        self.t6.start()
+
+    def terminar(self):
+        self.rodando = False
+        sleep(5)
+        print("Quantidade de Biodiesel produzido:", self.biodieselGerado)
+        print("Quantidade de glicerina produzida:", self.quantGlicerina)
+        print("Quantidade de Oleo restante:", self.quantOleo)
+        print("Quantidade de NaOH restante:", self.quantNaOh)
+        print("Quantidade de EtOH restante:", self.quantEtOh)
+        print("Quantidade de ciclos do reator:", self.ciclosReator)
+                
 
 
 simulador = Simulador()
+simulador.iniciar()
+sleep(3200)
+simulador.terminar()
 
-t1 = Thread(target = simulador.tanqOlRec)
-t1.start()
-
-t2 = Thread(target= simulador.recebeNaOheEtOH)
-t2.start()
-
-t3 = Thread(target=simulador.ativaReator)
-t3.start()
-
-t4 = Thread(target=simulador.ativaDecantador)
-t4.start()
